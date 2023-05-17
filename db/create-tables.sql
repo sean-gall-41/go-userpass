1def703dd576c0fd0dac6e23af20e5614bf35617
Sean Gallogly <galloglyst149@gmail.com>
2023-05-17 18:42:40 -0500

    playing with creating tables and querying

---
+++ db/create-tables.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  id INT AUTO_INCREMENT NOT NULL,
+  email VARCHAR(128) NOT NULL,
+  username VARCHAR(64) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+/*
+INSERT INTO `go-userpass-users`
+  (email, username, password_hash)
+VALUES
+  (...)
+  .
+  .
+  .
+  (...);
+
+this is what we will write as a string into go
+in order to insert users into our database
+*/
--- index.go
+++ index.go
@@ -1,9 +1,78 @@
 package main
 
 import(
+  "database/sql"
+  "os"
+  "log"
   "fmt"
+  "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
+type User struct {
+  ID int64
+  Email string
+  Username string
+  PassHash string
+}
+
+func queryUsersByEmail(email string) ([]User, error) {
+  var users []User
+  rows, err := db.Query(
+    "SELECT * FROM users WHERE email = ?",
+    email,
+  )
+  if err != nil {
+    return nil, fmt.Errorf("queryUserByEmail %q: %v", email, err)
+  }
+  defer rows.Close()
+  for rows.Next() {
+    var usr User
+    if err := rows.Scan(&usr.ID, &usr.Email, &usr.Username, &usr.PassHash); err != nil {
+      return nil, fmt.Errorf("queryUserByEmail %q: %v", email, err)
+    }
+    users = append(users, usr)
+  }
+  if err := rows.Err(); err != nil {
+    return nil, fmt.Errorf("queryUserByEmail %q: %v", email, err)
+  }
+  return users, nil
+}
+
 func main() {
-  fmt.Println("hello, seaman!")
+  dbName := "go-userpass-users"
+  fmt.Printf("Attempting to connect to database %s\n", dbName)
+  cfg := mysql.Config {
+    User: os.Getenv("DBUSER"),
+    Passwd: os.Getenv("DBPASS"),
+    Net: "unix",
+    Addr: "/var/run/mysqld/mysqld.sock",
+    DBName: dbName,
+    AllowNativePasswords: true,
+  }
+  var err error
+  db, err = sql.Open("mysql", cfg.FormatDSN())
+  if err != nil {
+    log.Fatal(err)
+  }
+  pingErr := db.Ping()
+  if pingErr != nil {
+    log.Fatal(pingErr)
+  }
+  fmt.Printf("Connection Succesful.\n")
+
+  users, err := queryUsersByEmail("myemail@hotmail.com")
+  if err != nil {
+    log.Fatal(err)
+  }
+  if len(users) > 1 {
+    fmt.Errorf("Bruh you messed up the database: two users with same email? NAH lil bro")
+    os.Exit(1)
+  }
+  if users == nil {
+    fmt.Printf("no users found :(\n")
+  } else {
+    fmt.Printf("user found: %v\n", users[0])
+  }
 }
